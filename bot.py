17635a4b895ab373e85e0055ed8b6dc59198d166
Marco Placentino <marco.placentino@edu.unito.it>
2019-09-01 19:10:19 +0200

    Aggiunti codici unicode per emoji miglorata funzione tempo

--- bot.py
+++ bot.py
@@ -1,9 +1,22 @@
 import time,logging,requests,telebot,_json
 
+# Openweathermap Weather codes and corressponding emojis
+thunderstorm = u'\U0001F4A8'    # Code: 200's, 900, 901, 902, 905
+drizzle = u'\U0001F4A7'         # Code: 300's
+rain = u'\U00002614'            # Code: 500's
+snowflake = u'\U00002744'       # Code: 600's snowflake
+snowman = u'\U000026C4'         # Code: 600's snowman, 903, 906
+atmosphere = u'\U0001F301'      # Code: 700's foogy
+clearSky = u'\U00002600'        # Code: 800 clear sky
+fewClouds = u'\U000026C5'       # Code: 801 sun behind clouds
+clouds = u'\U00002601'          # Code: 802-803-804 clouds general
+hot = u'\U0001F525'             # Code: 904
+defaultEmoji = u'\U0001F609'    # default emojis
+
+###
 TOKEN = "938633123:AAGsU0pSh2vsLJNS71q9V9OKZLlMjBnbWpg"
 bot = telebot.TeleBot(token=TOKEN)
-defaultEmoji = u'\U0001F609'
-thermometer= u'\U0001F321'
+
 
 
 def findat(msg):
@@ -57,26 +70,47 @@ def send_weather(message):
     print("User:"+message.from_user.first_name +" Text: "+message.text)
     print(x)
     if x["cod"] != "404":
-        y = x["main"]
-        current_temperature =int(y["temp"])-273,15
-        max_temperature = int(y["temp_max"])-273,15
-        min_temperature = int(y["temp_min"])-273,15
-        current_humidiy = y["humidity"]
-        z = x["weather"]
-        weather_description = z[0]["description"]
-        bot_response=("Temperatura attuale:  " +str(thermometer)+" "+
-                    str(current_temperature) +" °C"+
-                    "\nTemperatura max : " +str(thermometer)+" "+
-                    str(max_temperature) +" °C"+
-                    "\nTemperatura min : " +str(thermometer)+" "+
-                    str(min_temperature) +" °C"+
-                    "\nUmidità :  " +
-                    str(current_humidiy) +" %"
-                    "\nInfo :  " +
-                        str(weather_description))
+        cityName = x.get('name')
+        countryName = x.get('sys').get('country')
+        temp_current = int(x.get('main').get('temp'))-273,15
+        max_temperature =int(x.get('main').get('temp_max'))-273,15
+        min_temperature = int(x.get('main').get('temp_min'))-273,15
+        description_brief = x.get('weather')[0].get('main')
+        weatherID = x.get('weather')[0].get('id')     # gets ID of weather description, used for emoji
+        emoji = getEmoji(weatherID)
+        bot_response= cityName + ', ' + countryName + ': ' + str(temp_current) + '°C\n' + 'Max: ' + str(max_temperature) + '°C - ' + 'Min: ' + str(min_temperature)+ '°C\n' +   description_brief +str(emoji) + str(emoji)
         bot.reply_to(message,bot_response)
     else:
         bot.reply_to(message,"City not found")
+
+"""
+    Return related emojis according to weather
+"""
+def getEmoji(weatherID):
+    if weatherID:
+        if str(weatherID)[0] == '2' or weatherID == 900 or weatherID==901 or weatherID==902 or weatherID==905:
+            return thunderstorm
+        elif str(weatherID)[0] == '3':
+            return drizzle
+        elif str(weatherID)[0] == '5':
+            return rain
+        elif str(weatherID)[0] == '6' or weatherID==903 or weatherID== 906:
+            return snowflake + ' ' + snowman
+        elif str(weatherID)[0] == '7':
+            return atmosphere
+        elif weatherID == 800:
+            return clearSky
+        elif weatherID == 801:
+            return fewClouds
+        elif weatherID==802 or weatherID==803 or weatherID==803:
+            return clouds
+        elif weatherID == 904:
+            return hot
+        else:
+            return defaultEmoji    # Default emoji
+
+    else:
+        return defaultEmoji   # Default emoji
 
 
 while True:
